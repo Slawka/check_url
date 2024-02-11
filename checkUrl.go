69e568e86864239efe8aed21e9875c1765a79f90
Slawka <slawka@eth0.org.ru>
2024-02-11 23:32:28 +0300

    Edit Exec command

--- checkUrl.go
+++ checkUrl.go
@@ -80,7 +80,7 @@ $ checkUrl FileConfig.conf
 
 			out, err := cmd.Output()
 
-			log.Println(strings.Split(conf.Command, " ")[0]," ", strings.Join(strings.Split(conf.Command, " ")[1:], " "))
+			log.Println(strings.Split(conf.Command, " ")[0], strings.Join(strings.Split(conf.Command, " ")[1:], " "))
 
 			log.Println("out:", outb.String(), "err:", errb.String())
 			log.Println("out:", out, "err:", err)
