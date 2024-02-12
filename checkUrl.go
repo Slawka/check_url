fb54ca935c44d8d0b8bed5d487e682f9e230cd5f
Slawka <slawka@eth0.org.ru>
2024-02-12 22:25:24 +0300

    Edit Exec command

--- checkUrl.go
+++ checkUrl.go
@@ -85,7 +85,7 @@ $ checkUrl FileConfig.conf
 			log.Println(strings.Split(conf.Command, " ")[0], strings.Join(strings.Split(conf.Command, " ")[1:], " "))
 
 			// log.Println("out:", outb.String(), "err:", errb.String())
-			// log.Println("out:", out, "err:", err)
+			log.Println("out:", cmd.Stdout, "err:", err)
 			// cmd := exec.Command("/usr/bin/echo", "`/usr/bin/date +\"%Y/%M/%d %H:%m:%S\"`\" Http reboot \"", ">>","/var/log/httpd/restart-nginx.log")
 			// cmd.Run()
 			break
